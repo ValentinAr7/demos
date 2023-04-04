31f01aaca4858d98ca80b7953923338a69341b04
Valentin <valio990210@gmail.com>
2023-04-04 12:34:57 +0300

    demo

--- app/demo.js
+++ app/demo.js
@@ -1,25 +1,23 @@
-function tip(){
+function tip() {
+    let amount = document.querySelector('#input_bill').value;
+    let people = document.querySelector('#input_people').value;
+    let service = document.querySelector('#services').value;
 
-    let amount = document.querySelector('input_bill').value
-    let people = document.querySelector('input_people').value
-    let service = document.querySelector('services').value
-
-    if(amount == '' || people == ''){
-        alert('Please entry valid values')
-    return
-    } 
-
-    if(people == '1'){
-        document.querySelector('each').style.display = none;
-    } else {
-        document.querySelector('each').style.display = 'block'
+    if (amount === '' || people === '') {
+        alert('Please enter valid values');
+        return;
     }
 
-    let total = (amount * service) / people
-    total = total.toFixed(2)
-    
-    document.querySelector('tip').style.display = 'block'
-    document.querySelector('total').innerHTML = total
-}
+    if (people === '1') {
+        document.querySelector('#each').style.display = 'none';
+    } else {
+        document.querySelector('#each').style.display = 'block';
+    }
 
+    let total = (amount * service) / people;
+    total = total.toFixed(2);
+    
+    document.querySelector('.tip').style.display = 'block';
+    document.querySelector('#total').innerHTML = total;
+}
 
